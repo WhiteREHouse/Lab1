9578ffcbe2af3d6b530521cf4e872c456a82561a
WhiteREHouse <61017011+WhiteREHouse@users.noreply.github.com>
2020-09-12 17:43:13 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.lab1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,43 +20,48 @@ class MainActivity : AppCompatActivity() {
         view = ActivityMainBinding.inflate(layoutInflater)
         setContentView(view.root)
 
+        view.actionPlus.setOnClickListener{ execute('+') }
 
+        view.actionMinus.setOnClickListener{ execute('-')}
 
-        view.actionPlus.setOnClickListener{ if(onAnyActionClick()){
-            view.resultOutputWidget.text = (view.firstNumber.text.toString().toDouble() + view.secondNumber.text.toString().toDouble()).toString() }}
+        view.actionMultiply.setOnClickListener{ execute('*') }
 
-        view.actionMinus.setOnClickListener{ if(onAnyActionClick()){
-            view.resultOutputWidget.text = (view.firstNumber.text.toString().toDouble() - view.secondNumber.text.toString().toDouble()).toString() }}
-
-        view.actionMultiply.setOnClickListener{ if(onAnyActionClick()){
-            view.resultOutputWidget.text = (view.firstNumber.text.toString().toDouble() * view.secondNumber.text.toString().toDouble()).toString() }}
-
-        view.actionDivide.setOnClickListener{ if(onAnyActionClick()){
-            if(view.secondNumber.text.toString().toDouble()==0.0){
-                view.divideByZeroWarning.visibility = View.VISIBLE
-            }
-            else
-                view.resultOutputWidget.text = (view.firstNumber.text.toString().toDouble() / view.secondNumber.text.toString().toDouble()).toString()
-        }}
-
+        view.actionDivide.setOnClickListener{ execute('/') }
 
 
     }
-    fun onAnyActionClick(): Boolean{
+    fun execute(operation:Char) {
         view.resultOutputWidget.text = ""
-        view.divideByZeroWarning.visibility = View.INVISIBLE
-        view.firstNumberEmptyWarning.visibility = View.INVISIBLE
-        view.secondNumberEmptyWarning.visibility = View.INVISIBLE
-        var bool = true
-        if(view.firstNumber.text.toString() == ""){
-            view.firstNumberEmptyWarning.visibility = View.VISIBLE
-            bool = false
+        if(view.firstNumber.text.toString() == ""||view.secondNumber.text.toString() == ""){
+            val toast =Toast.makeText(applicationContext, "Fill both fields!", Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.CENTER,0,0)
+            toast.show()
+            return
         }
-        if(view.secondNumber.text.toString() == ""){
-            view.secondNumberEmptyWarning.visibility = View.VISIBLE
-            bool = false
+        var result = 0.0
+        val first = view.firstNumber.text.toString().toDouble()
+        val second = view.secondNumber.text.toString().toDouble()
+        when {
+            operation.equals('*') -> {
+                result = first * second
+            }
+            operation.equals('+') -> {
+                result = first + second
+            }
+            operation.equals('-') -> {
+                result = first - second
+            }
+            operation.equals('/') -> {
+                if(view.secondNumber.text.toString().toDouble()==0.0){
+                    val toast =Toast.makeText(applicationContext, "Cannot divide by 0!", Toast.LENGTH_SHORT)
+                    toast.setGravity(Gravity.CENTER,0,0)
+                    toast.show()
+                    return
+                }
+                result = first / second
+            }
         }
-        return bool
+        view.resultOutputWidget.text = result.toString()
     }
 
 }
